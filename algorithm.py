de4ad2cbb7a09a3a58d887f11524ced430321892
Jack Goler <jack.goler@gmail.com>
2021-04-21 17:17:27 -0700

    continued working on it

--- algorithm.py
+++ algorithm.py
@@ -1,8 +1,16 @@
 #the board is 7 long by 6 high
 
-gameboard = [[0, 1, 2, 3, 4, 5],[6,7,8,9,10,11],[12,13,14,15,16,17],[18,19,20,21,22,23],[24,25,26,27,28,29],[30,31,32,33,34,35],[36,37,38,39,40,41]]
-def play(gameboard):
-	print("play function")
+gameboard = [[0, 0, 0, 0, 0, 0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
+def play(gameboard, first):
+	print("The player that will go first is player", first)
+	#print("{b1}! This is {b2}.".format(b1 = n1, b2 = n2))
+	xcoord = input("What is the x-coordinate of where you want to go? ")
+	ycoord = input("And what is the y-coordinate of where you want to go? ")
+	#print("{b1},{b2}".format(b1=xcoord, b2=ycoord))
+	if gameboard[int(xcoord)][int(ycoord)] != 0:
+		print("That position is already taken, please choose a different position")
+	else:
+		gameboard[int(xcoord)][int(ycoord)] = first
 
 def print_board(gameboard):
 	column = 0
@@ -14,7 +22,7 @@ def print_board(gameboard):
 		column += 1
 
 
-print_board(gameboard)
+play(gameboard, 1)
 
 #def test():
 	#tester = [[1,2,3], [4,5,6],[7,8,9]]
