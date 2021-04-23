a9dc79dcda0e280631abca6edbc31fb102a795ff
Jack Goler <jack.goler@gmail.com>
2021-04-23 14:52:39 -0700

    worked on it a bit

--- algorithm.py
+++ algorithm.py
@@ -7,20 +7,35 @@ def play(gameboard, first):
 	xcoord = input("What is the x-coordinate of where you want to go? ")
 	ycoord = input("And what is the y-coordinate of where you want to go? ")
 	#print("{b1},{b2}".format(b1=xcoord, b2=ycoord))
-	if gameboard[int(xcoord)][int(ycoord)] != 0:
+	if gameboard[int(xcoord)-1][int(ycoord)-1] != 0:
 		print("That position is already taken, please choose a different position")
 	else:
-		gameboard[int(xcoord)][int(ycoord)] = first
+		gameboard[int(xcoord)-1][int(ycoord)-1] = first
+	print_board(gameboard)
+
+
+def is_winner(gameboard):
+	return true
 
 def print_board(gameboard):
-	column = 0
-	while column < 7:
-		row = 0
-		while row < 6:
-			print(gameboard[column][row])
-			row += 1
-		column += 1
+	row = 0
+	#print('__________________')
+	print('_______________')
+	while row < 6:
+		current_pos = gameboard[0][5-row]
+		second_pos = gameboard[1][5-row]
+		third_pos = gameboard[2][5-row]
+		fourth_pos = gameboard[3][5-row]
+		fifth_pos = gameboard[4][5-row]
+		sixth_pos = gameboard[5][5-row]
+		seventh_pos = gameboard[6][5-row]
+		#print("|{n1}|".format(n1=current_pos)
+		print("|{b1}|{b2}|{b3}|{b4}|{b5}|{b6}|{b7}|".format(b1 = current_pos, b2 = second_pos, b3=third_pos, b4=fourth_pos, b5=fifth_pos, b6=sixth_pos, b7=seventh_pos))
+		print('_______________')
+		row += 1
 
+def test(gameboard):
+	print(gameboard[6][0])
 
 play(gameboard, 1)
 
