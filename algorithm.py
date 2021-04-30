7c2b4bbfe7fed4c7a09a3671b403018c76b30228
Jack Goler <jack.goler@gmail.com>
2021-04-30 14:30:29 -0700

    working on determening a winner

--- algorithm.py
+++ algorithm.py
@@ -1,21 +1,36 @@
 #the board is 7 long by 6 high
 
 gameboard = [[0, 0, 0, 0, 0, 0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
+
+def winner_exists(gameboard):
+	row = 0
+	while (row < 6):
+		print("here in while (row < 6):")
+		if int(gameboard[row][0]) == int(gameboard[row][1]) == int(gameboard[row][2]) == int(gameboard[row][3]) and int(gameboard[row][3]) != 0:
+			return True
+		elif int(gameboard[row][1]) == int(gameboard[row][2]) == int(gameboard[row][3]) == int(gameboard[row][4]) and int(gameboard[row][4]) != 0:
+			return True
+		elif int(gameboard[row][2]) == int(gameboard[row][3]) == int(gameboard[row][4]) == int(gameboard[row][5]) and int(gameboard[row][5]) != 0:
+			return True
+		row += 1
+	return False
+
 def play(gameboard, first):
-	print("The player that will go first is player", first)
-	#print("{b1}! This is {b2}.".format(b1 = n1, b2 = n2))
-	xcoord = input("What is the x-coordinate of where you want to go? ")
-	ycoord = input("And what is the y-coordinate of where you want to go? ")
-	#print("{b1},{b2}".format(b1=xcoord, b2=ycoord))
-	if gameboard[int(xcoord)-1][int(ycoord)-1] != 0:
-		print("That position is already taken, please choose a different position")
-	else:
-		gameboard[int(xcoord)-1][int(ycoord)-1] = first
-	print_board(gameboard)
+	print("here")
+	print(winner_exists(gameboard))
+	while (not winner_exists(gameboard)):
 
+		print("The player that will go first is player", first)
+		#print("{b1}! This is {b2}.".format(b1 = n1, b2 = n2))
+		xcoord = input("What is the x-coordinate of where you want to go? ")
+		ycoord = input("And what is the y-coordinate of where you want to go? ")
+		#print("{b1},{b2}".format(b1=xcoord, b2=ycoord))
+		if gameboard[int(xcoord)-1][int(ycoord)-1] != 0:
+			print("That position is already taken, please choose a different position")
+		else:
+			gameboard[int(xcoord)-1][int(ycoord)-1] = first
+		print_board(gameboard)
 
-def is_winner(gameboard):
-	return true
 
 def print_board(gameboard):
 	row = 0
@@ -38,6 +53,10 @@ def test(gameboard):
 	print(gameboard[6][0])
 
 play(gameboard, 1)
+print(winner_exists(gameboard))
+
+
+
 
 #def test():
 	#tester = [[1,2,3], [4,5,6],[7,8,9]]
