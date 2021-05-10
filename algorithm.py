d9c62c626f0114588de0cdb722e2635af09e4806
Jack Goler <jack.goler@gmail.com>
2021-05-10 14:45:57 -0700

    did alternating turns

--- algorithm.py
+++ algorithm.py
@@ -41,52 +41,45 @@ def winner_exists(gameboard):
 
 
 def play(gameboard, first):
-	print("here")
-	print(winner_exists(gameboard))
-	while (not winner_exists(gameboard)):
-
-		print("The player that will go first is player", first)
-		#print("{b1}! This is {b2}.".format(b1 = n1, b2 = n2))
-		xcoord = input("What is the x-coordinate of where you want to go? ")
-		ycoord = input("And what is the y-coordinate of where you want to go? ")
-		#print("{b1},{b2}".format(b1=xcoord, b2=ycoord))
-		if gameboard[int(ycoord)-1][int(xcoord)-1] != 0:
-			print("That position is already taken, please choose a different position")
-		else:
-			gameboard[int(ycoord)-1][int(xcoord)-1] = first
-		print_board(gameboard)
+    current_player = first
+    print(winner_exists(gameboard))
+    while (not winner_exists(gameboard)):
+        print("Please make your move player ", current_player)
+        #print("{b1}! This is {b2}.".format(b1 = n1, b2 = n2))
+        xcoord = input("What is the x-coordinate of where you want to go? ")
+        ycoord = input("And what is the y-coordinate of where you want to go? ")
+        #print("{b1},{b2}".format(b1=xcoord, b2=ycoord))
+        if gameboard[int(ycoord)-1][int(xcoord)-1] != 0:
+            print("That position is already taken, please choose a different position")
+        else:
+            gameboard[int(ycoord)-1][int(xcoord)-1] = current_player
+            if current_player == 1:
+                current_player = 2
+            else:
+                current_player = 1
+        print_board(gameboard)
 
 
 def print_board(gameboard):
-	row = 5
-	#print('__________________')
-	print('_______________')
-	while row >= 0:
-		current_pos = gameboard[row][0]
-		second_pos = gameboard[row][1]
-		third_pos = gameboard[row][2]
-		fourth_pos = gameboard[row][3]
-		fifth_pos = gameboard[row][4]
-		sixth_pos = gameboard[row][5]
-		seventh_pos = gameboard[row][6]
-		#print("|{n1}|".format(n1=current_pos)
-		print("|{b1}|{b2}|{b3}|{b4}|{b5}|{b6}|{b7}|".format(b1 = current_pos, b2 = second_pos, b3=third_pos, b4=fourth_pos, b5=fifth_pos, b6=sixth_pos, b7=seventh_pos))
-		print('_______________')
-		row -= 1
+    row = 5
+    #print('__________________')
+    print('_______________')
+    while row >= 0:
+        current_pos = gameboard[row][0]
+        second_pos = gameboard[row][1]
+        third_pos = gameboard[row][2]
+        fourth_pos = gameboard[row][3]
+        fifth_pos = gameboard[row][4]
+        sixth_pos = gameboard[row][5]
+        seventh_pos = gameboard[row][6]
+        #print("|{n1}|".format(n1=current_pos)
+        print("|{b1}|{b2}|{b3}|{b4}|{b5}|{b6}|{b7}|".format(b1 = current_pos, b2 = second_pos, b3=third_pos, b4=fourth_pos, b5=fifth_pos, b6=sixth_pos, b7=seventh_pos))
+        print('_______________')
+        row -= 1
 
 def test(gameboard):
-	print(gameboard[6][0])
+    print(gameboard[6][0])
 
 print_board(gameboard)
 play(gameboard, 1)
 print(winner_exists(gameboard))
-
-
-
-
-#def test():
-	#tester = [[1,2,3], [4,5,6],[7,8,9]]
-	#print(tester[2][2])
-
-
-#test()
