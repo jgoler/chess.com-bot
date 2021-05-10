4995e2d7b805be22ed8ca580d48911cc63d020f8
Jack Goler <jack.goler@gmail.com>
2021-05-10 14:33:28 -0700

    got it working

--- algorithm.py
+++ algorithm.py
@@ -5,7 +5,6 @@ def winner_exists(gameboard):
     row = 0
     x_value = 0
     while (row < 6 and x_value < 4):
-        print("horizontal check")
         if (int(gameboard[row][x_value]) == int(gameboard[row][x_value + 1])) and (int(gameboard[row][x_value]) == int(gameboard[row][x_value + 2])) and (int(gameboard[row][x_value]) == int(gameboard[row][x_value + 3])) and (int(gameboard[row][x_value]) != 0):
             return True
         if (x_value == 3):
@@ -17,7 +16,6 @@ def winner_exists(gameboard):
     row = 0
     x_value = 0
     while (row < 3 and x_value < 7):
-        print("vertical check")
         if (int(gameboard[row][x_value]) == int(gameboard[row + 1][x_value])) and (int(gameboard[row][x_value]) == int(gameboard[row + 2][x_value])) and (int(gameboard[row][x_value]) == int(gameboard[row + 3][x_value])) and (int(gameboard[row][x_value]) != 0):
             return True
         if (row == 2 and x_value != 6):
@@ -28,7 +26,6 @@ def winner_exists(gameboard):
     row = 0
     x_value = 0
     while (row < 3 and x_value < 7):
-        print("diagonal check")
         if (x_value < 4):
             if (int(gameboard[row][x_value]) == int(gameboard[row + 1][x_value + 1])) and (int(gameboard[row][x_value]) == int(gameboard[row + 2][x_value + 2])) and (int(gameboard[row][x_value]) == int(gameboard[row + 3][x_value + 3])) and (int(gameboard[row][x_value]) != 0):
                 return True
@@ -53,33 +50,34 @@ def play(gameboard, first):
 		xcoord = input("What is the x-coordinate of where you want to go? ")
 		ycoord = input("And what is the y-coordinate of where you want to go? ")
 		#print("{b1},{b2}".format(b1=xcoord, b2=ycoord))
-		if gameboard[int(xcoord)-1][int(ycoord)-1] != 0:
+		if gameboard[int(ycoord)-1][int(xcoord)-1] != 0:
 			print("That position is already taken, please choose a different position")
 		else:
-			gameboard[int(xcoord)-1][int(ycoord)-1] = first
+			gameboard[int(ycoord)-1][int(xcoord)-1] = first
 		print_board(gameboard)
 
 
 def print_board(gameboard):
-	row = 0
+	row = 5
 	#print('__________________')
 	print('_______________')
-	while row < 6:
-		current_pos = gameboard[0][5-row]
-		second_pos = gameboard[1][5-row]
-		third_pos = gameboard[2][5-row]
-		fourth_pos = gameboard[3][5-row]
-		fifth_pos = gameboard[4][5-row]
-		sixth_pos = gameboard[5][5-row]
-		seventh_pos = gameboard[6][5-row]
+	while row >= 0:
+		current_pos = gameboard[row][0]
+		second_pos = gameboard[row][1]
+		third_pos = gameboard[row][2]
+		fourth_pos = gameboard[row][3]
+		fifth_pos = gameboard[row][4]
+		sixth_pos = gameboard[row][5]
+		seventh_pos = gameboard[row][6]
 		#print("|{n1}|".format(n1=current_pos)
 		print("|{b1}|{b2}|{b3}|{b4}|{b5}|{b6}|{b7}|".format(b1 = current_pos, b2 = second_pos, b3=third_pos, b4=fourth_pos, b5=fifth_pos, b6=sixth_pos, b7=seventh_pos))
 		print('_______________')
-		row += 1
+		row -= 1
 
 def test(gameboard):
 	print(gameboard[6][0])
 
+print_board(gameboard)
 play(gameboard, 1)
 print(winner_exists(gameboard))
 
